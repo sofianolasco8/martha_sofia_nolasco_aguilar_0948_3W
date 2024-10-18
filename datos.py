c866b6f36e8e8efdc221c8a290be9b15c63d3d77
sofianolasco8 <a.23308051140948@cbtis128.edu.mx>
2024-10-18 00:15:40 -0600

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,27 @@
+print(" ")
+print("nolasco_aguilar_martha_sofia_0948_3w")
+print(" ")
+# abri un diccionario vacío
+datos = {}
+
+# indicar al usuario ingresar su informacion
+nombre = input("Ingresa tu nombre: ")
+datos["Nombre"] = nombre
+print(datos)
+
+edad = input("Ingresa tu edad: ")
+datos["Edad"] = edad
+print(datos)
+
+sexo = input("Ingresa tu sexo: ")
+datos["Sexo"] = sexo
+print(datos)
+
+telefono = input("Ingresa tu teléfono: ")
+datos["Teléfono"] = telefono
+print(datos)
+
+correo = input("Ingresa tu correo electrónico: ")
+datos["Correo Electrónico"] = correo
+print(datos)
+
